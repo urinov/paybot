01b8ea875ead03ffa7d6e54cb8d62f34784f84ee
urinov <30720428+urinov@users.noreply.github.com>
2025-09-16 00:05:48 +0500

    Update clickSign.js

--- utils/clickSign.js
+++ utils/clickSign.js
@@ -1,3 +1,4 @@
+// utils/clickSign.js
 import crypto from 'crypto';
 
 export function buildPrepareSign({ click_trans_id, service_id, secret_key, merchant_trans_id, amount, action, sign_time }) {
@@ -9,4 +10,3 @@ export function buildCompleteSign({ click_trans_id, service_id, secret_key, merc
   const s = '' + click_trans_id + service_id + secret_key + merchant_trans_id + merchant_prepare_id + amount + action + sign_time;
   return crypto.createHash('md5').update(s, 'utf8').digest('hex');
 }
-
